6167361ed0d4b2aa09983c4f6a830090703a3a4a
talhaahmed884 <52800026+talhaahmed884@users.noreply.github.com>
2021-06-16 19:08:31 +0500

    Removed grabage

--- foodiefox/lib/backend/validator/CustomerValidate.dart
+++
@@ -1,7 +0,0 @@
-import 'package:foodiefox/backend/validator/Validate.dart';
-
-abstract class CustomerValidate extends Validate {
-  bool validateCellNo(String cellNo);
-
-  bool validateName(String name);
-}
